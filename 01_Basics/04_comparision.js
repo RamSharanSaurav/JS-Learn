e3682b7abed3261c1e0daa66de1c7834f74958aa
RamSharanSaurav <112314743+RamSharanSaurav@users.noreply.github.com>
2024-04-10 10:57:42 +0000

    comparision overview

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,18 @@
+// Basic conversion
+// console.log(1 >= 5);
+// console.log(5 < 10);
+
+
+// Bad practive problem in conversion
+// Avoid these kind of conversions
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
