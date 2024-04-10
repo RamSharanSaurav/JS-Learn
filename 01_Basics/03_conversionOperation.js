a810938f841705830be73b19af0dc2c0202ed07d
RamSharanSaurav <112314743+RamSharanSaurav@users.noreply.github.com>
2024-04-10 10:03:51 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let  valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false = 0
+
+
+let isLoggedIn = "1"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Saurav" => true
+
+let someNumnber = 33
+
+let stringNumber = String(someNumnber)
+console.log(typeof stringNumber);
+
