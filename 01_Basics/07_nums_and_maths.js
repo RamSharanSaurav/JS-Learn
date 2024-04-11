349a3f1ce5f666ee968225a1dd2debef546602e7
Tech_Geek_Saurav <112314743+RamSharanSaurav@users.noreply.github.com>
2024-04-11 12:33:26 +0000

    maths & nums

---
+++ 01_Basics/07_nums_and_maths.js
@@ -0,0 +1,38 @@
+const score = 389
+// console.log(score);
+
+const balance = new Number(356)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(5));
+
+const anotherNumber = 123.48667549
+
+// console.log(anotherNumber.toPrecision(2));
+
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ======================== Maths ========================//
+
+// console.log(Math);
+// console.log(Math.abs(-245));
+// console.log(Math.round(3.98679576));
+// console.log(Math.ceil(5.436584395));
+// console.log(Math.floor(6.86755));
+// console.log(Math.sqrt(3));
+// console.log(Math.min(3,4,6,1,7,8));
+// console.log(Math.max(3,9,3,5,1,4));
+
+console.log(Math.random());
+console.log((Math.random() * 100) + 1);
+console.log(Math.floor(Math.random() * 100) + 1);
+
+const min = 100
+const max = 200
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
