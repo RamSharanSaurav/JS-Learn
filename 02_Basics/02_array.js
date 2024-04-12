2a7ce4f75d52c7fd915c704d1668ffd5e8c4f2c8
Tech_Geek_Saurav <112314743+RamSharanSaurav@users.noreply.github.com>
2024-04-12 11:52:39 +0000

    array methods

--- 02_Basics/02_array.js
+++ 02_Basics/02_array.js
@@ -17,4 +17,22 @@ const allHeros = marvelHeros.concat(dcHeros)
 // Spread Method
 
 const allNewHeros = [...marvelHeros, ...dcHeros]
-console.log(allNewHeros);
+// console.log(allNewHeros);
+
+// Falt in array
+
+const arr = [1, 2, 3, [4, 5, 6], 7, [8, [9, 10]]]
+
+// console.log(arr.flat(Infinity));
+// console.log(arr.flat(2));
+
+
+console.log(Array.isArray("Saurav"));
+console.log(Array.from("Saurav"));
+console.log(Array.from({name: "Saurav"})); // Intresting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
